af74ec31e589261a0c7d492bb1c7724b3687c7b7
Yeasin00p <07Yeasin1981@gmail.com>
2024-05-13 08:49:26 +0600

    drawer

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  //logout user
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //drawer header
+          DrawerHeader(
+            child: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          Column(
+            children: [
+              //home tittle
+              HomeListTile(
+                iconData: Icons.home,
+                text: 'H O M E',
+                onTap: () {
+                  //this is allready home screen so just pop drawer
+                  Navigator.pop(context);
+                },
+              ),
+              //profile tittle
+              HomeListTile(
+                iconData: Icons.person,
+                text: 'P R O F I L E',
+                onTap: () {},
+              ),
+              //user tittle
+              HomeListTile(
+                iconData: Icons.group,
+                text: 'U S E R S',
+                onTap: () {},
+              ),
+            ],
+          ),
+          HomeListTile(
+            iconData: Icons.logout,
+            text: 'L O G O U T ',
+            onTap: logout,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeListTile extends StatelessWidget {
+  final IconData iconData;
+  final String text;
+  final void Function()? onTap;
+  const HomeListTile({
+    super.key,
+    required this.iconData,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: ListTile(
+        leading: Icon(
+          iconData,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        title: Text(text),
+        onTap: onTap,
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:s_media_app/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  //logout user
-  void logout() {
-    FirebaseAuth.instance.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
-          ),
-        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
